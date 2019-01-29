e03b02e55188e6ac1a1712770f1dbfa6ad80fc39
admirallightningbolt <aviknecht@gmail.com>
2019-01-29 11:54:55 -0800

    Solved problem 3.

---
+++ euler/p3_largest_prime_factor/p3_largest_prime_factor.py
@@ -0,0 +1,42 @@
+# Find the largest prime factor of some number.
+
+import argparse
+import math
+import os
+import sys
+
+# This bit is ridiculous
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+
+def prime_factors_brute(n, max_iter=1000000):
+  primes = []
+  powers = []
+  remainder = n
+  number = 2
+  while remainder != 1:
+    if number > max_iter:
+      break
+    if remainder % number == 0:
+      primes.append(number)
+      power = 1
+      remainder = remainder / number
+      # Keep trying the same number until it stops dividing.
+      while remainder % number == 0:
+        power += 1
+        remainder = remainder / number
+      powers.append(power)
+      print("Remainder: %s. Prime: %s. Power: %s." % (remainder, number, power))
+    number += 1
+  print()
+  return primes, powers
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Sum some.")
+  parser.add_argument("--num", default=600851475143, type=int, help="Number to factor.")
+  parser.add_argument("--iter", default=1000000, type=int, help="Max iterations.")
+  args = parser.parse_args()
+  primes, powers = prime_factors_brute(args.num, args.iter)
+  print("Primes: %s \nPowers: %s" % (primes, powers))
